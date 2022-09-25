9d4d6d98a26d89783d020db0ab7e1d2352254081
Saul Lee <saul@atomrigs.io>
2022-09-25 11:39:15 +0900

    Initial Commit

---
+++ git-xcode1/git_xcode1App.swift
@@ -0,0 +1,17 @@
+//
+//  git_xcode1App.swift
+//  git-xcode1
+//
+//  Created by 아톰릭스랩 on 2022/09/25.
+//
+
+import SwiftUI
+
+@main
+struct git_xcode1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
